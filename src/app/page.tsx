f54dd72c3fecdc3191b6ff9fae920883f07de856
Akhil Waghmare <akhilwaghmare@gmail.com>
2024-09-21 18:39:17 -0700

    Add basic content without optimizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,29 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <div className="flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)] bg-neutral-200">
+      <header className="px-8 py-4 flex justify-between items-center">
+        <h1 className="font-semibold text-xl">Built With Code</h1>
+        <span className="uppercase text-sm">Menu</span>
+      </header>
+      <main className="relative flex flex-1 flex-col gap-6 sm:gap-12 justify-center items-center p-8 sm:py-16">
+        <h1 className="text-3xl sm:text-6xl font-semibold text-center">
+          Learn to craft web interfaces.
+        </h1>
+        <button className="px-5 py-2 bg-black rounded-full font-semibold text-white">
+          Get started
+        </button>
+        <div className="relative h-[30vh] max-w-[90%] aspect-video rounded-lg overflow-hidden shadow-md">
+          <img
+            className="object-cover"
+            src="/bg-image.jpg"
+            alt="Built With Code"
+          />
+        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+      <footer className="flex items-center justify-center text-sm px-8 py-4 text-neutral-700 text-center">
+        Copyright © 2024 Built With Code.
+      </footer>
     </div>
   );
 }
